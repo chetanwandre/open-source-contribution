45b434627f156f221dc0a5f52d234939d1636b59
Chetan Wandre <chetanwandre2005@gmail.com>
2025-07-21 18:09:58 +0530

    done1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("LMAO")
+
+// console.log('some meaningful contributions');
+
+// objects
+const newObj = {
+    name: "rahu",
+    pass: "ketu",
+    age: 69 
+}
+
+console.log(newObj.name);
+
+// normal function declaration
+function greeting() {
+    console.log('have some meaningful contributions on Github');
+    
+}
+
+// the changes made after creating new repo
